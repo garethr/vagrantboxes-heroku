fe40b1e0b98ec82f31c202d238c453b6947e5c5d
Jean-Sebastien Ney <jeansebastien.ney@gmail.com>
2012-08-27 13:09:07 +0200

    sort boxes by type & by name before display

--- src/controller.rb
+++ src/controller.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 
 before 'index.html.erb' do
-  @boxes = YAML.load_file('data/boxes.yml')
+  @boxes = YAML.load_file('data/boxes.yml').
+    sort{|x, y| x[:name] <=> y[:name]}.
+    sort{|x, y| x[:type] <=> y[:type]}
 end
