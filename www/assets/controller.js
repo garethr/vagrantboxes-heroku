09b883a695df21b772be37b541428d812e1b439c
jmhallst <jm.rodriguez@hallst.com>
2014-06-18 16:59:06 +0200

    Terminado

--- www/assets/controller.js
+++ www/assets/controller.js
@@ -1,28 +1,41 @@
-angular.module('VagrantBoxes', [
-  'VagrantBoxes.services',
-  'VagrantBoxes.controllers'
-]);
-
-angular.module('VagrantBoxes.services', [])
-
-  .factory('jsonAPIservice', function($http) {
-    var jsonAPI = {};
-
-    jsonAPI.getBoxes = function() {
-      return $http({
-        method: 'GET', 
-        url: 'manifest.json'
-      });
-    }
-
-    return jsonAPI;
-  });    
-    
 angular.module('VagrantBoxes.controllers', [])
+
+  .directive('modalDialog', function() {
+      return {
+        restrict: 'E',
+        scope: {
+          show: '='
+        },
+        replace: true, // Replace with the template below
+        transclude: true, // we want to insert custom content inside the directive
+        link: function(scope, element, attrs) {
+          scope.dialogStyle = {};
+          if (attrs.width)
+            scope.dialogStyle.width = attrs.width;
+          if (attrs.height)
+            scope.dialogStyle.height = attrs.height;
+          scope.hideModal = function() {
+            scope.show = false;
+          };
+        },
+        template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+      };
+  })
 
   .controller('boxesController', function($scope, jsonAPIservice) {
     $scope.modalShown = false;
+    
     jsonAPIservice.getBoxes().success(function (response) {
         $scope.boxList = response;
     });
+
+    $scope.getTextToCopy = function(url) {
+        console.log(url);
+        return url;
+    }
+        
+    $scope.toggleModal = function(box) {
+        $scope.modalShown = !$scope.modalShown;
+        $scope.actualBox = box;
+    };    
   });
